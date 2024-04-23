e3ae7053122e08acd839e32cb5100691dec41eae
Eduardo Folly <edufolly@gmail.com>
2024-04-23 17:21:50 -0300

    Updating tests.

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
-set -e
-set -x
+
+# set -e
+# set -x
+
+code=0
 
 docker build . --no-cache -t snmpd-container-test
 
@@ -8,8 +11,24 @@ docker run --rm --name snmpd -p 5161:161/udp -d snmpd-container-test
 
 sleep 2
 
-snmpwalk -v 2c -c public localhost:5161 .
+snmpwalk -v 2c -c public localhost:5161 .1.3.6.1.2.1.1
+
+location=$(snmpget -v2c -c public -Ovq localhost:5161 .1.3.6.1.2.1.1.6.0)
+
+if [ "$location" != "At flying circus" ]; then
+    echo "Location is not correct."
+    code=10
+fi
+
+services=$(snmpgetnext -v2c -c public -Ovq localhost:5161 .1.3.6.1.2.1.1.6.0)
+
+if [ "$services" != "72" ]; then
+    echo "Services is not correct."
+    code=11
+fi
 
 docker stop -t 1 snmpd
 
 docker image rm snmpd-container-test
+
+exit $code
